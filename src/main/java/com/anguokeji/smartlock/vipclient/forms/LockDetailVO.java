94f2f5d6f5fc704ee97c40389983ab07c4d113fc
DengZhiGuo <bobdeng@163.com>
2019-11-13 17:23:24 +0800

    add name

--- src/main/java/com/anguokeji/smartlock/vipclient/forms/LockDetailVO.java
+++ src/main/java/com/anguokeji/smartlock/vipclient/forms/LockDetailVO.java
@@ -4,6 +4,15 @@ public class LockDetailVO {
     private String id;
     private String mac;
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     private NBDeviceVO nb;
 
     public String getId() {
